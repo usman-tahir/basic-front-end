53ceb5ca6d494ec910a2b9d4f84d93c698b31f85
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-05-04 23:43:06 -0400

    added scss compilation and updated  gulp task definitions

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,34 @@
 	'use strict';
 
 	var gulp = require('gulp'),
-		browserSync = require('browser-sync').create();
+		browserSync = require('browser-sync').create(),
+		sass = require('gulp-sass');
 
-	gulp.task('browser-sync', function () {
+	// gulp.task('browser-sync', function () {
+	// 	browserSync.init({
+	// 		server: {
+	// 			baseDir: './'
+	// 		}
+	// 	});
+	// });
+
+	// Create the static server, and watch SASS/HTML files
+	gulp.task('serve', ['sass'], function () {
 		browserSync.init({
-			server: {
-				baseDir: './'
-			}
+			server: './app'
 		});
+
+		gulp.watch('app/scss/*.scss', ['sass']);
+		gulp.watch('app/*.html').on('change', browserSync.reload);
 	});
+
+	// Compile sass into CSS, and inject it into the browser
+	gulp.task('sass', function () {
+		return gulp.src('app/scss/*.scss')
+			.pipe(sass())
+			.pipe(gulp.dest('app/css'))
+			.pipe(browserSync.stream())
+	});
+
+	gulp.task('default', ['serve']);
 }());
